5d70f53189036fdc10477114fc490c94e70c5582
yuanzicheng <997171257@qq.com>
2020-07-22 17:44:20 +0800

    mukuindex

---
+++ mukuofficialwebsite/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+<el-container>
+  <el-header>Header</el-header>
+  <el-main>Main</el-main>
+  <el-footer>Footer</el-footer>
+</el-container>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
