07dd67917455aced8692a34afe5e1fe8779ff8ab
rime-188 <1449068059@qq.com>
2020-07-24 12:21:32 +0800

    about

--- mukuofficialwebsite/src/main.js
+++ mukuofficialwebsite/src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+import axios from 'axios'
 Vue.config.productionTip = false
-
+axios.defaults.baseURL = 'http://127.0.0.1:30002'
+Vue.prototype.$http = axios
 new Vue({
   router,
   render: h => h(App)
--- mukuofficialwebsite/src/views/About.vue
+++ mukuofficialwebsite/src/views/About.vue
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="//api.map.baidu.com/api?v=2.0&ak=GBsu1G2zGYFS1BrFvnMEizHFsgvj74nX"></script>
 <template>
   <div class="about">
     <el-container>
@@ -20,15 +21,56 @@
             class="content"
           >幕库公司的主要服务和合作对象包括口岸物流相关单位、同济大学建筑信息学院、小英科技、铁实信息、迦楠建设等。幕库以为客户提供更优质的服务为业务宗旨，坚持以人为本、科技先行的管理理念，围绕新一代信息技术跨界多元经营，竭诚为新老客户提供支持与服务。</p>
           <p class="content"></p>
-          <p align="center" style="font-size: 16px;">公司地址:{{address}}</p>
-          <div id="mapPicture">
-            <img src="../assets/mapPicture01.jpg" /><img src="../assets/mapPicture02.jpg" alt />
+          <p align="center" style="font-size: 16px;">公司地址:{{SysCorp.address}}</p>
+          <div id="mapDiv" align="center">
+            <p><span style="margin-right: 250px;">普通图</span><span style=" margin-left: 250px">卫星图</span></p>
+            <baidu-map
+              class="map"
+              :center="center"
+              :zoom="zoom"
+              @ready="handler"
+              :scroll-wheel-zoom="true"
+            >
+              <bm-marker
+                :position="{lng:121.932756, lat: 30.897182}"
+                :dragging="true"
+                animation="BMAP_ANIMATION_BOUNCE"
+              >
+                <bm-label
+                  content="上海幕库科技发展有限公司"
+                  :labelStyle="{color: 'black', fontSize : '12px'}"
+                  :offset="{width: -35, height: 30}"
+                />
+              </bm-marker>
+            </baidu-map>
+            <baidu-map
+              class="map"
+              :center="center"
+              :zoom="zoom"
+              @ready="handler"
+              :scroll-wheel-zoom="true"
+              mapType="BMAP_SATELLITE_MAP"
+            >
+              <bm-marker
+                :position="{lng: 121.932756, lat: 30.897182}"
+                :dragging="true"
+                animation="BMAP_ANIMATION_BOUNCE"
+              >
+                <bm-label
+                  content="上海幕库科技发展有限公司"
+                  :labelStyle="{color: 'black', fontSize : '8px'}"
+                  :offset="{width: -35, height: 30}"
+                />
+              </bm-marker>
+            </baidu-map>
+            <!-- <img src="../assets/mapPicture01.jpg" /><img src="../assets/mapPicture02.jpg" alt /> -->
           </div>
         </div>
       </el-main>
     </el-container>
   </div>
 </template>
+
 <style>
 .content {
   text-indent: 2em;
@@ -44,23 +86,56 @@
   width: 100%;
   height: 100%;
 }
-#mapPicture {
-  width: 1000px;
-  height: 1000px;
+#mapDiv {
+  margin-top: 100px;
+  align-items: center;
 }
-#mapPicture > img {
-    width: 300px;
-    height: 300px;
-    margin: 100px;
+.map {
+  width: 500px;
+  height: 500px;
+}
+#mapDiv > .map {
+  margin: 50px;
+  margin-top: 0;
+  display: inline-block;
 }
 </style>
 <script>
+import Vue from "vue";
+import BaiduMap from "vue-baidu-map";
+
+Vue.use(BaiduMap, {
+  ak: "GBsu1G2zGYFS1BrFvnMEizHFsgvj74nX",
+  dragging: true,
+});
 export default {
-  data () {
+  data() {
     return {
-      company: {},
-      address: '上海浦东新区'
-    }
-  }
-}
+      center: { lng: 0, lat: 0 },
+      zoom: 3,
+      SysCorp: {},
+    };
+  },
+  methods: {
+    searchSysCorpOne() {
+      this.$http
+        .get("/websiteservice/sysCorp/searchSysCorpOne", {
+          withCredentials: true,
+        })
+        .then((Response) => {
+          this.SysCorp = Response.data.data;
+        })
+        .catch((err) => console.log(err));
+    },
+    handler({ BMap, map }) {
+      console.log(BMap, map);
+      this.center.lng = 121.9327563;
+      this.center.lat = 30.897182;
+      this.zoom = 18;
+    },
+  },
+  mounted() {
+    this.searchSysCorpOne();
+  },
+};
 </script>
