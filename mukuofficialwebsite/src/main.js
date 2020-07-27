7a512ababfba986023276422abbe44c98936f1af
you <129635237@qq.com>
2020-07-27 10:32:23 +0800

    提交优秀案例接口联调

--- mukuofficialwebsite/src/main.js
+++ mukuofficialwebsite/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import './plugins/element.js'
 import axios from 'axios'
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://127.0.0.1:30002'
+axios.defaults.baseURL = 'http://119.23.71.198:30002'
 Vue.prototype.$http = axios
 new Vue({
   router,
